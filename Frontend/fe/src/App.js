ca2d482df685ddc2a3419118eac5946da6029cee
Vaish-011 <muskantomar43@gmail.com>
2025-02-07 13:28:23 +0530

    "Initialised"

---
+++ Frontend/fe/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
+import Login from './components/Login/login'; 
+import Create from './components/CreateAccount/create'; 
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Login />} /> 
+          <Route path="/create-account" element={<Create />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
